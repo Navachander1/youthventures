486eeaaf6a5547df6be9ef785fe0e502f34fa24c
Navachander1 <navachander@graduate.utm.my>
2023-12-10 20:32:37 +0800

    delete

--- metronic_html_v8.2.1_demo37/tools/webpack/plugins/custom/draggable/draggable.js
+++
@@ -1,4 +0,0 @@
-// Draggable - a lightweight, responsive, modern drag & drop library: https://shopify.github.io/draggable/
-
-import Draggable from '@shopify/draggable/build/umd';
-window.Draggable = Draggable;
